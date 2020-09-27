23aea82f70d78179275cef59307ee6f78334f353
751682 <751682@SAIT224907IT.ACDM.DS.SAIT.CA>
2020-09-27 14:00:29 -0600

    Converted application into a full MVC pattern

--- src/java/servlets/HelloWorldServlet.java
+++ src/java/servlets/HelloWorldServlet.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import models.Person;
 
 /**
  *
@@ -25,24 +26,20 @@ public class HelloWorldServlet extends HttpServlet {
         // get the two parameters from the POST request
         String firstname = request.getParameter("firstname");
         String lastname = request.getParameter("lastname");
-        
-        // set the attributes for the JSP
-        request.setAttribute("firstname", firstname);
-        request.setAttribute("lastname", lastname);
-        
-        // validation: if the parameters don't exist or are empty, show the first page again
+         
         if (firstname == null || firstname.equals("") || lastname == null || lastname.equals("")) {
-            // set an attribute for a message
+            request.setAttribute("firstname", firstname);
+            request.setAttribute("lastname", lastname);
             request.setAttribute("message", "Invalid entry. Please enter both your first and last names.");
             
-            // forward the request and response objects to the JSP
-            // display the form again
             getServletContext().getRequestDispatcher("/WEB-INF/helloWorldForm.jsp")
                     .forward(request, response);
             return; // very important!
         }
 
-        // display the second page
+        Person person = new Person(firstname, lastname);       
+        request.setAttribute("person", person);
+        
         getServletContext().getRequestDispatcher("/WEB-INF/sayHello.jsp")
                 .forward(request, response);
     }
--- web/WEB-INF/sayHello.jsp
+++ web/WEB-INF/sayHello.jsp
@@ -13,6 +13,6 @@
     </head>
     <body>
         <h1>Hello World!</h1>
-        <h2>Hello ${firstname} ${lastname}</h2>
+        <h2>Hello ${person.firstname} ${person.lastname}</h2>
     </body>
 </html>
